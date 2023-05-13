c5c56cc9c565b7a0eabe55f806fbe252dbe5e016
ashutosh <ashutoshsangra@gmail.com>
2023-05-13 16:41:13 +0530

    First commit

---
+++ TutorialApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TutorialApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
